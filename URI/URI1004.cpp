a757843037996a8525916c5f1b0118b2c22e69ea
Matheus-Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-09-27 17:45:23 -0300

    URI 1004

---
+++ URI/URI1004.cpp
@@ -0,0 +1,20 @@
+/*
+  Matheus dos Santos Ribeiro Silva
+  URI 1004 - "Simple Product"
+  27/09/2021
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int a, b;
+
+  cin >> a >> b;
+
+  cout << "PROD = " << (a * b) << endl;
+
+  return 0;
+}
