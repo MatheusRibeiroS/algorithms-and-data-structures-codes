306e828b45e689d652561767a0bc9659fff39c28
Matheus-Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-09-27 17:47:01 -0300

    URI 1007

---
+++ URI/URI1007.cpp
@@ -0,0 +1,20 @@
+/*
+  Matheus dos Santos Ribeiro Silva
+  URI 1007 - "Difference"
+  27/09/2021
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int A, B, C, D;
+
+  cin >> A >> B >> C >> D;
+
+  cout << "DIFERENCA = " << ((A * B) - (C * D)) << endl;
+
+  return 0;
+}
