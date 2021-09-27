dc20d8535ef4744a26509b0c2b11ac5118f9bfa5
Matheus-Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-09-27 17:44:58 -0300

    URI 1003

---
+++ URI/URI1003.cpp
@@ -0,0 +1,20 @@
+/*
+  Matheus dos Santos Ribeiro Silva
+  URI 1003 - "Simple Sum"
+  27/09/2021
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int A, B;
+
+  cin  >> A >> B;
+
+  cout << "SOMA = " << (A + B) << endl;
+
+  return 0;
+}
