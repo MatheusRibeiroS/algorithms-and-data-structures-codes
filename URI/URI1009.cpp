e6b1991ce55d77a1668fce50ad375743113aa049
Matheus-Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-09-27 17:47:41 -0300

    URI 1009

---
+++ URI/URI1009.cpp
@@ -0,0 +1,24 @@
+/*
+  Matheus dos Santos Ribeiro Silva
+  URI 1009 - "Salary with Bonus"
+  27/09/2021
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+  string sellerName;
+  double salary, totalSale;
+
+  cin >> sellerName >> salary >> totalSale;
+
+  cout << fixed << setprecision(2);
+
+  cout << "TOTAL = R$ " << salary + (totalSale * 0.15) << endl;
+
+  return 0;
+}
