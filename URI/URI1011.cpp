73ac37fb51049716ec8a1cfde9ce46007de70f34
Matheus-Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-09-27 23:43:40 -0300

    URI 1011

---
+++ URI/URI1011.cpp
@@ -0,0 +1,25 @@
+/*
+  Matheus dos Santos Ribeiro Silva
+  URI 1011 - "Sphere"
+  27/09/2021
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+  int radius;
+  const double PI = 3.14159;
+
+  cin >> radius;
+
+  cout << fixed << setprecision(3);
+
+  cout << "VOLUME = " << (4.0/3.0) * PI * radius * radius * radius << endl;
+
+  return 0;
+}
+
