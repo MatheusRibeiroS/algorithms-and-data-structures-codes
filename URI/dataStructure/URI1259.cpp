445331dadad7a15203d2bfbe0026aa7de6b9a35e
Matheus-Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-10-04 14:22:22 -0300

    URI 1259

---
+++ URI/dataStructure/URI1259.cpp
@@ -0,0 +1,39 @@
+/*
+  Matheus dos Santos Ribeiro Silva
+  URI 1259 - "Even and Odd"
+  04/10/2021
+*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+  int n, i;
+
+  cin >> n;
+
+  int number[n];
+
+  for(i=0; i<n; i++) {
+    cin >> number[i];
+  }
+
+  sort(number, number + n);
+
+  for(i = 0; i < n; i++) {
+    if(number[i] % 2 == 0) {
+      cout << number[i] << endl;
+    } 
+  }
+
+  for(i = n-1; i >= 0; i--) {
+    if(number[i] % 2 != 0) {
+      cout << number[i] << endl;
+    } 
+  }
+
+  return 0;
+}
