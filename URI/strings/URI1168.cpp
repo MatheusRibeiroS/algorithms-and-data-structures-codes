c84238b867792501ecf56e108b1f0d8508e89fe1
Matheus-Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-10-05 14:01:47 -0300

    URI 1168

---
+++ URI/strings/URI1168.cpp
@@ -0,0 +1,49 @@
+/*
+  Matheus dos Santos Ribeiro Silva
+  URI 1168 - "LED"
+  05/10/2021
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int n, cont = 0;
+
+  cin >> n;
+
+  for(int i = 0; i<n; i++) {
+    cont = 0;
+    string led;
+    cin >> led;
+
+    for(int j = 0; j<led.length(); j++) { 
+      if(led[j] == '1') {
+        cont+= 2;
+      } else if(led[j] == '2') {
+        cont+= 5;
+      } else if(led[j] == '3') {
+        cont+= 5;
+      } else if(led[j] == '4') {
+        cont+= 4;
+      } else if(led[j] == '5') {
+        cont+= 5;
+      } else if(led[j] == '6') {
+        cont+= 6;
+      } else if(led[j] == '7') {
+        cont+= 3;
+      } else if(led[j] == '8') {
+        cont+= 7;
+      } else if(led[j] == '9') {
+        cont+= 6;
+      } else if(led[j] == '0') {
+        cont+= 6;
+      }
+    }
+    cout << cont << " leds" << endl;
+  }
+  
+  return 0;
+}
