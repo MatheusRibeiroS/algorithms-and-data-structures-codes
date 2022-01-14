d31a972010d060de5dae9215440acb7d5341c0f3
Matheus-Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2022-01-13 23:48:10 -0300

    URI 2606

---
+++ URI/SQL/URI2606.sql
@@ -0,0 +1,8 @@
+/*
+  Matheus dos Santos Ribeiro Silva
+  URI 2606 - "Categories"
+  13/01/2022
+*/
+
+SELECT p.id, p.name FROM products as p INNER JOIN
+categories as c ON p.id_categories = c.id WHERE c.name LIKE 'super%';
